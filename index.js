01a2ed4e53cbfaecea7ae91bcf7ed945ded75510
Nadun Dhananjaya <nadundhananjaya98@yahoo.com>
2023-04-21 19:04:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ import cors from 'cors'
 
 const app = express()
 const env =dotenv.config();
-app.use(cors())
+app.use(cors({
+    origin: '*'
+}));
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
